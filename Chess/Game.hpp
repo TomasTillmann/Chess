5faacd2e7cc3c350b68ccc927c989b857ee482ff
Toams Tillmann <tomas.tilllmann@gmail.com>
2023-04-23 16:27:03 +0200

    added game

---
+++ Chess/Game.hpp
@@ -0,0 +1,43 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "Position.hpp"
+#include "MoveGenerator.hpp"
+#include "Debug.hpp"
+#include <algorithm>
+
+class Game {
+private:
+	Position _position;
+
+public:
+	Game()
+		: _position(Position::get_starting()) {
+	}
+
+	color_t player_to_play() const
+	{
+		return _position.to_play();
+	}
+
+	Position current_position() const {
+		return _position;
+	}
+
+	void make_move(move_t move) {
+#if DEBUG
+		std::vector<move_t> legal_moves = MoveGenerator::generate_legal_moves(_position);
+		if (std::find(legal_moves.begin(), legal_moves.end(), move) == legal_moves.end()) {
+			throw std::invalid_argument("illegal move");
+		}
+#endif
+
+		_position.make_move(move);
+	}
+
+	std::vector<move_t> legal_moves() const {
+		return MoveGenerator::generate_legal_moves(_position);
+	}
+};
+
+#endif
